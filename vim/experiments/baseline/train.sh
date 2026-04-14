3ed2dd029d8f2cbcdc935369c40a46c97ed4ce0e
darpalp <darpalpatel123@gmail.com>
2026-04-14 01:50:39 -0600

    scripts

--- vim/experiments/baseline/train.sh
+++ vim/experiments/baseline/train.sh
@@ -5,14 +5,14 @@
 
 MODEL="vim_base_patch16_224_bimambav2_final_pool_mean_abs_pos_embed_with_middle_cls_token_div2"
 FINETUNE_CKPT="checkpoints/vim_b_midclstok_81p9acc.pth"
-DATA_PATH="./imagenet"
+DATA_PATH="imagenet"
 OUTPUT_DIR="checkpoints/baseline_vim_b"
 
 env -u SLURM_PROCID python vim/main.py \
   --model $MODEL \
   --finetune $FINETUNE_CKPT \
   --data-set IMNET --data-path $DATA_PATH \
-  --batch-size 32 --lr 5e-6 --min-lr 1e-7 \
+  --batch-size 64 --lr 5e-5 --min-lr 1e-6 \
   --weight-decay 1e-8 --warmup-epochs 5 \
   --drop-path 0.0 --epochs 10 \
   --eval-batch-size 64 \
